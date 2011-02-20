8e6a30967e52e4aca100d79c6036affab7d723fd
Ryan Bubinski <ryanbubinski@gmail.com>
2011-02-20 14:05:08 -0500

    Set PRAGMA default_synchronous=OFF for SQLITE write speed improvement

--- crawl.rb
+++ crawl.rb
@@ -5,11 +5,18 @@ Bundler.require(:default)
 require 'active_record'
 require "./config"
 
+
 # Configure ActiveRecord
-ActiveRecord::Base.establish_connection(
-  :adapter  => "sqlite3",
-  :database => $database_filename # set in config.db
-)
+  ActiveRecord::Base.establish_connection(
+    :adapter  => "sqlite3",
+    :database => $database_filename # set in config.db
+  )
+  # Disable sycnchronous writes for performance gain
+  # NOTE: Database may be corrupted if computer power is lost mid-crawl
+  c = ::ActiveRecord::Base.connection
+  c.execute("PRAGMA default_synchronous=OFF")
+  c.execute("PRAGMA synchronous=OFF")
+
 
 # Load models
 Dir["./lib/*.rb"].each {|file| require file }
